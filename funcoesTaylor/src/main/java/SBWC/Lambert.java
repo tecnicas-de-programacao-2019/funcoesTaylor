66d3243012db4de28095f4ef31d0784bfdb6ecd3
Silvio <silviopeixe@hotmail.com>
2019-04-08 20:15:19 -0300

    Classe Lambert foi criado classe lambert e não foi testado por silvio

---
+++ funcoesTaylor/src/main/java/SBWC/Lambert.java
@@ -0,0 +1,20 @@
+package SBWC;
+import java.util.ArrayList;
+
+public class Lambert {
+
+    private Util util;
+    public double wo(double x){
+        double breakPoint = Math.pow(10, -12);
+        double result;
+        int n=0;
+    ArrayList<Double> lista = new ArrayList<Double>();
+    do{
+        result = ((Math.pow((-n), n-1))/util.fatorial(n) * Math.pow(x,n));
+        n++;
+        lista.add(result);
+    }while(Math.abs(result)>breakPoint);
+
+    return util.somatorio(0, n, lista);
+    }
+}
