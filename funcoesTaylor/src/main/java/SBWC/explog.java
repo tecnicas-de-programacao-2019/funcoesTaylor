e142ef5b4c20d13ce7f6afed96079d31b772cb40
william antunes rubert <william.rubert@gmail.com>
2019-04-08 16:20:33 -0300

    funcoes exp

---
+++ funcoesTaylor/src/main/java/SBWC/explog.java
@@ -0,0 +1,40 @@
+package SBWC;
+
+import java.util.ArrayList;
+
+public class Trig {
+
+    private Util util;
+
+	public double ex(double x){
+		double breakPoint = Math.pow(10, -12);
+
+        double result;
+        int n = 0;
+        ArrayList<Double> lista = new ArrayList<Double>();
+		
+		 do{
+            result = ((Math.pow(x, n))/util.fatorial(n));
+            n++;
+            lista.add(result);
+        }while(Math.abs(result) > breakPoint);
+
+        return util.somatorio(0, n, lista);
+    }
+	
+    public double lnx(double x){
+        double breakPoint = Math.pow(10, -12);
+
+        double result;
+        int n = 0;
+        ArrayList<Double> lista = new ArrayList<Double>();
+        do{
+            result = ((Math.pow(-1, n))/(n+1)) * Math.pow(x, (n+1));
+            n++;
+            lista.add(result);
+        }while(Math.abs(result) > breakPoint);
+
+        return util.somatorio(0, n, lista);
+    }
+	
+}
