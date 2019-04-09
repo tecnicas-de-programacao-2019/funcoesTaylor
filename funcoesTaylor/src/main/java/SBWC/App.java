3778675088bbf8478125e1809abff2337dfc58b8
18109228 <18109228@facin.portoalegre.pucrsnet.br>
2019-04-09 09:24:26 -0300

    App pronto

--- funcoesTaylor/src/main/java/SBWC/App.java
+++ funcoesTaylor/src/main/java/SBWC/App.java
@@ -5,8 +5,15 @@ public class App
     public static void main( String[] args )
     {
         Trig trig = new Trig();
-        //60° em rad
-        System.out.println( trig.cos(1.0472) );
-        System.out.println( trig.sin(1.0472) );
+	explog exp = new explog();
+	Lambert lamb = new Lambert();
+        
+	//60° em rad
+        System.out.println( trig.cos(1.0472) ); //Resultado esperado = 0.5
+        System.out.println( trig.sin(1.0472) ); //Resultado esperado =  0.86
+	System.out.println( exp.ex(5) ); //Resultado esperado = 148.41
+	System.out.println( exp.ln(5) ); //Resultado esperado = 1.6
+	System.out.println( lamb.wo(1) ); //Resultado esperado ≅ 0,567143
+	
     }
 }
