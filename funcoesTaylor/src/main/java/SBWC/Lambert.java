4567efc9d6f33673a3660726421d3cb46b979bd2
Bruno Lippert <brunohlippert@gmail.com>
2019-04-09 11:13:33 -0300

    arrumando funcao lambert

--- funcoesTaylor/src/main/java/SBWC/Lambert.java
+++ funcoesTaylor/src/main/java/SBWC/Lambert.java
@@ -5,12 +5,12 @@ class Lambert {
 
     private Util util;
     public double wo(double x){
-        double breakPoint = Math.pow(10, -12);
+        double breakPoint = Math.pow(10, -14);
         double result;
-        int n=0;
+        int n=1;
     ArrayList<Double> lista = new ArrayList<Double>();
     do{
-        result = ((Math.pow((-n), n-1))/util.fatorial(n) * Math.pow(x,n));
+        result = (((Math.pow((-n), n-1))/util.fatorial(n)) * Math.pow(x,n));
         n++;
         lista.add(result);
     }while(Math.abs(result)>breakPoint);
